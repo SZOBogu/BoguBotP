c2eecc4af45d6e404e2b8e23bdbaccd18f63f2b7
Bogu <adambozek1@gmail.com>
2021-06-23 15:51:14 +0200

    Probes are being built.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -8,6 +8,17 @@ public class Bot extends DefaultBWListener {
         Game game = bwClient.getGame();
         Player player = game.self();
         game.drawTextScreen(20, 20, player.getName() +  " has " + player.minerals() + "minerals");
+
+        //TODO: check whether player.getUnits returns unit list of whole race or unit list player has at disposal
+        for(Unit unit : player.getUnits()){
+            UnitType unitType = unit.getType();
+            if(unitType.isBuilding() && !unitType.buildsWhat().isEmpty()){
+                UnitType unitTypeToTrain = unitType.buildsWhat().get(0);
+                if(game.canMake(unitTypeToTrain, unit)){
+                    unit.train(unitTypeToTrain);
+                }
+            }
+        }
     }
 
     public void onUnitComplete(Unit unit){
